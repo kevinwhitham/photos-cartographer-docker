5c6221d3932ef0a8104d4c51840427fc10aab99c
kevinwhitham <kevin.whitham@gmail.com>
2026-09-09 13:21:16 -0500

    Move app start from script to Dockerfile ENTRYPOINT
    If shell script launches app, it can not be stopped gracefully. ENTRYPOINT can be stopped gracefully

--- photos-cartographer-start.sh
+++ photos-cartographer-start.sh
@@ -2,6 +2,3 @@
 
 # Copy default settings file if it does not exist on the host
 cp --no-clobber /default-settings/photos-config-defaults.json /photos-cartographer-config/
-
-# Start the app
-photos-cartographer console --port 8766 --host $(hostname -I | awk '{print $1}')
