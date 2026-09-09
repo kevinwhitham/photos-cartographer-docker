982a27d5116bafc982f2145fe6283e1b22c65b0b
kevinwhitham <kevin.whitham@gmail.com>
2026-09-09 13:16:06 -0500

    Move config file link creation to Dockerfile

--- photos-cartographer-start.sh
+++ photos-cartographer-start.sh
@@ -3,8 +3,5 @@
 # Copy default settings file if it does not exist on the host
 cp --no-clobber /default-settings/photos-config-defaults.json /photos-cartographer-config/
 
-# The app will access the config file by a symlink
-ln -s /photos-cartographer-config/photos-config-defaults.json /app/photos-config-defaults.json
-
 # Start the app
 photos-cartographer console --port 8766 --host $(hostname -I | awk '{print $1}')
